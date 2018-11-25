4303174723db95baca85753108a4994c0c97ac3f
panzc001 <510825294@qq.com>
2018-11-25 13:21:07 +0800

    自动生成代码

---
+++ src/main/java/com/gsroot/bhyx/BhyxApplication.java
@@ -0,0 +1,12 @@
+package com.gsroot.bhyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhyxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhyxApplication.class, args);
+    }
+}
