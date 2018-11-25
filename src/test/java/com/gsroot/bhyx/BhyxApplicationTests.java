c80be98f2d70a0251fb48c6af43da26603e253e1
panzc001 <510825294@qq.com>
2018-11-25 13:34:33 +0800

    自动生成代码

---
+++ src/test/java/com/gsroot/bhyx/BhyxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gsroot.bhyx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BhyxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
